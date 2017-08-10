796471f899feb7d25b21d099f79ec27b4c4d2cce
weimuhua <weimuhua0812@126.com>
2017-08-11 00:19:04 +0800

    增加notification单独测试页面。

---
+++ app/src/main/java/wayne/me/androidodemo/notification/NotificationActivity.java
@@ -0,0 +1,16 @@
+package wayne.me.androidodemo.notification;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import wayne.me.androidodemo.R;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+
+}
