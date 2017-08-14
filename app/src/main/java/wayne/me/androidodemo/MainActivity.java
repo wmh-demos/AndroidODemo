a391162f72ca3b36a57a2d962f56227f218f7440
weimuhua <weimuhua0812@126.com>
2017-08-14 23:27:41 +0800

    根据 Google 官方demo实现的通知sample。

--- app/src/main/java/wayne/me/androidodemo/MainActivity.java
+++ app/src/main/java/wayne/me/androidodemo/MainActivity.java
@@ -1,14 +1,14 @@
 package wayne.me.androidodemo;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+
+import wayne.me.androidodemo.notification.NotificationActivity;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
@@ -43,35 +43,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
         switch (position) {
             case INDEX_TEST_NOTIFY:
-                notifyTest();
+                startActivity(new Intent(this, NotificationActivity.class));
                 break;
-        }
-    }
-
-    private void notifyTest() {
-        NotificationManager nm =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        String[] channelIds = new String[]{"my_channel_01", "my_channel_02", "my_channel_03"};
-
-        int importance = NotificationManager.IMPORTANCE_LOW;
-
-        for (int index = 0; index < channelIds.length; index++) {
-            String id = channelIds[index];
-            CharSequence name = getResources().getString(R.string.notify_test, id);
-            NotificationChannel channel = new NotificationChannel(id, name, importance);
-            channel.setShowBadge(true);
-
-            if (nm != null) {
-                nm.createNotificationChannel(channel);
-                Notification notification = new Notification.Builder(mContext, id)
-                        .setContentTitle("New Messages")
-                        .setContentText("This is notification " + (index + 1))
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .build();
-
-                nm.notify(1000 + index, notification);
-            }
         }
     }
 }
