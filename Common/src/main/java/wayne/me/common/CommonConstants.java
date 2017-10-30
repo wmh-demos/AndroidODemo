d9e49bb534838122e6928c67919cba09a94f6b0c
weimuhua <weimuhua0812@126.com>
2017-10-30 19:14:12 +0800

    add common module.

---
+++ Common/src/main/java/wayne/me/common/CommonConstants.java
@@ -0,0 +1,32 @@
+package wayne.me.common;
+
+public class CommonConstants {
+    public static final String PERM_NORMAL =
+            "wayne.me.androidodemo.perm.NORMAL";
+    public static final String PERM_SIGN =
+            "wayne.me.androidodemo.perm.SIGN";
+    public static final String PERM_SIGN_OR_SYSTEM =
+            "wayne.me.androidodemo.perm.SIGN_OR_SYSTEM";
+
+    public static final String ACTION_APP1_TEST_SELF =
+            "wayne.me.androidodemo.action.APP1_TEST_SELF";
+    public static final String ACTION_APP1_TEST_NORMAL =
+            "wayne.me.androidodemo.action.APP1_TEST_NORMAL";
+    public static final String ACTION_APP1_TEST_PERM_NORMAL =
+            "wayne.me.androidodemo.action.APP1_TEST_PERM_NORMAL";
+    public static final String ACTION_APP1_TEST_PERM_SIGN =
+            "wayne.me.androidodemo.action.APP1_TEST_PERM_SIGN";
+    public static final String ACTION_APP1_TEST_PERM_SIGN_OR_SYSTEM =
+            "wayne.me.androidodemo.action.APP1_TEST_PERM_SIGN_OR_SYSTEM";
+
+    public static final String ACTION_APP2_TEST_TARGET =
+            "wayne.me.androidodemo.action.APP2_TEST_TARGET";
+    public static final String ACTION_APP2_TEST_NORMAL =
+            "wayne.me.androidodemo.action.APP2_TEST_NORMAL";
+    public static final String ACTION_APP2_TEST_PERM_NORMAL =
+            "wayne.me.androidodemo.action.APP2_TEST_PERM_NORMAL";
+    public static final String ACTION_APP2_TEST_PERM_SIGN =
+            "wayne.me.androidodemo.action.APP2_TEST_PERM_SIGN";
+    public static final String ACTION_APP2_TEST_PERM_SIGN_OR_SYSTEM =
+            "wayne.me.androidodemo.action.APP2_TEST_PERM_SIGN_OR_SYSTEM";
+}
